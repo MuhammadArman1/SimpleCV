d19fb6eca339c45db775b8838dbbc5f2b31e2c27
muhammadarman: armanimran801@gmail.com <armanimran801@gmail.com>
2024-10-08 16:01:27 +0500

    simple-cv

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -5,13 +5,13 @@ export default function Nav() {
   return (
     <div>
         <nav id="nav" className="flex justify-center h-16 items-center text-xl font-bold">
-      <ul className="flex justify-around gap-8 mr-16">
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/education">Education</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
-      </ul>
-    </nav>
+            <ul className="flex justify-around gap-8 mr-16">
+               <li><Link href="/">Home</Link></li>
+               <li><Link href="/about">About</Link></li>
+               <li><Link href="/education">Education</Link></li>
+               <li><Link href="/contact">Contact</Link></li>
+            </ul>
+        </nav>
     </div>
   )
 }
